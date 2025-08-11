d6a9ecf9dab204625cb23036c9efcd8836fb8ecd
bobjoerules <yo.bob.joe.rules@gmail.com>
2025-08-11 11:41:10 -0700

    Added More custom images

--- keyword.js
+++ keyword.js
@@ -16,6 +16,30 @@ function processMatches(root) {
             el.innerHTML = `<img width="20px" src="images/x.jpg" class="icon" alt="">${text}`;
             updatedCells.add(el);
         }
+        if (text.toLowerCase() === "tiktok".toLowerCase() || text.toLowerCase().startsWith("tiktok")) {
+            el.innerHTML = `<img width="20px" src="images/TikTok.png" class="icon" alt="">${text}`;
+            updatedCells.add(el);
+        }
+        if (text.toLowerCase() === "diamond".toLowerCase() || text.toLowerCase().startsWith("diamond")) {
+            el.innerHTML = `<img width="20px" src="images/Diamond.png" class="icon" alt="">${text}`;
+            updatedCells.add(el);
+        }
+        if (text.toLowerCase() === "fortnite".toLowerCase() || text.toLowerCase().startsWith("fortnite")) {
+            el.innerHTML = `<img width="20px" src="images/Fortnite.png" class="icon" alt="">${text}`;
+            updatedCells.add(el);
+        }
+        if (text.toLowerCase() === "genshin".toLowerCase() || text.toLowerCase().startsWith("genshin")) {
+            el.innerHTML = `<img width="20px" src="images/genshin.png" class="icon" alt="">${text}`;
+            updatedCells.add(el);
+        }
+        if (text.toLowerCase() === "minecraft".toLowerCase() || text.toLowerCase().startsWith("minecraft")) {
+            el.innerHTML = `<img width="20px" src="images/minecraft.svg" class="icon" alt="">${text}`;
+            updatedCells.add(el);
+        }
+        if (text.toLowerCase() === "roblox".toLowerCase() || text.toLowerCase().startsWith("roblox")) {
+            el.innerHTML = `<img width="20px" src="images/Roblox.png" class="icon" alt="">${text}`;
+            updatedCells.add(el);
+        }
     });
 }
 
